067cf77912be1b771c56dd5fcd4009ca646ca75b
guyunderwood <85255257+guyunderwood@users.noreply.github.com>
2022-05-26 14:38:03 +0700

    messing with index.js

---
+++ react-basic/src/components/HelloComponent3.js
@@ -0,0 +1,5 @@
+function HelloComponent3() {
+  return <h1>Hello Component by external file</h1>;
+}
+
+export default HelloComponent3;
--- react-basic/src/index.js
+++ react-basic/src/index.js
@@ -3,13 +3,27 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import HelloComponent3 from './components/HelloComponent3';
+
+function HelloComponent(){
+  return <h1>Hello Component by function</h1>
+}
+
+class HelloComponent2 extends React.Component{    // needed to start with capital letter
+  render(){
+    return <li>Hello Component by class</li>
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <HelloComponent/>
+  // </HelloComponent2>
+  <HelloComponent3/>
+  
 );
+
+// ReactDOM.render(<HelloComponent2/>, document.getElementById('root'));   // can't use this syntax?????
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
