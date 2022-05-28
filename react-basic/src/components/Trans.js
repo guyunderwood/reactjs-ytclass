c6ed3b3ebcff89baef64b28924cb8333474bb4e1
guytanakrit <72789875+guytanakrit@users.noreply.github.com>
2022-05-29 00:27:31 +0700

    use uuid

--- react-basic/src/components/Trans.js
+++ react-basic/src/components/Trans.js
@@ -1,19 +1,21 @@
 import Item from "./Item";
 import './Trans.css'
+import { v4 as uuidv4 } from 'uuid';
+
 const Trans = () => {
   const data = [
-    {id:1, title:'Medical bill', amount:2000},
-    {id:2, title: 'Gas', amount: 5000},
-    {id:3, title: 'Rental', amount: 8000},
-    {id:4, title: 'Payroll', amount: 70000},
-    {id:5, title: 'Insurance', amount: 300},
-    {id:6, title: 'Travel expense', amount: 800}
+    {title:'Medical bill', amount:2000},
+    {title: 'Gas', amount: 5000},
+    {title: 'Rental', amount: 8000},
+    {title: 'Payroll', amount: 70000},
+    {title: 'Insurance', amount: 300},
+    {title: 'Travel expense', amount: 800}
   ]
   return (
     <ul className="item-list">
       {data.map((element)=>{
         // return <Item title={element.title} amount={element.amount}/>
-        return <Item {...element} key = {element.id}/>   // same as above
+        return <Item {...element} key = {uuidv4()}/>   // same as above
       })} 
     </ul>
   );
